d6ffd7fb6fda3ec3e54536cb0b2511197b95b08a
Abdul Basit Ansari <abdulbasitab8888@gmail.com>
2026-04-28 21:37:22 +0500

    update CORS

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://frontend-tau-ten-50.vercel.app",  
+        "https://shop-api-velmora.vercel.app",  
     ],
     allow_credentials=True,
     allow_methods=["*"],
